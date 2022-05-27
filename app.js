47c41dfaf4dff2174d73ab3bc5742db67ab45fd0
Kevin Blair <kblair40@gmail.com>
2022-05-27 16:01:50 -0500

    Add cors package and use with app

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require("express");
-const mongoPwd = require("./ignore");
+const cors = require("cors");
 const mongoose = require("mongoose");
+const mongoPwd = require("./ignore");
+
+require("./models");
+
+const routes = require("./routes");
 
 const app = express();
 
 // replacement for body-parser which shouldn't be needed in express 4.16+
 app.use(express.json());
+app.use(cors());
+app.use(routes);
 
 const mongoUri = `mongodb+srv://kblair40:${mongoPwd}@cluster0.gwipufa.mongodb.net/?retryWrites=true&w=majority`;
 mongoose.connect(mongoUri, { useNewUrlParser: true });
--- routes.js
+++ routes.js
@@ -2,13 +2,20 @@ const express = require("express");
 
 const router = express.Router();
 
+router.post("/guest", async (req, res) => {
+  //
+  const { body } = req;
+  console.log("\n\nBODY:", body, "\n\n");
+  return res.status(200).send({ msg: "success" });
+});
+
 router.get("/guest", async (req, res) => {
   try {
     console.log("SENDING:", url);
-    res.send({ msg: "success" });
+    res.status(200).send({ msg: "success" });
   } catch (err) {
     console.log("ERROR WITH S3:", err);
-    res.send({ msg: "success" });
+    res.status(422).send({ msg: "failure" });
   }
 });
 
