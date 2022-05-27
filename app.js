fad69ce8d8eaff324c2b83beefd717f2d6c09f8f
Kevin Blair <kblair40@gmail.com>
2022-05-27 15:19:51 -0500

    Get mongoose/mongo connection working

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require("express");
+const mongoPwd = require("./ignore");
+const mongoose = require("mongoose");
+
 const app = express();
 
 // replacement for body-parser which shouldn't be needed in express 4.16+
 app.use(express.json());
+
+const mongoUri = `mongodb+srv://kblair40:${mongoPwd}@cluster0.gwipufa.mongodb.net/?retryWrites=true&w=majority`;
+mongoose.connect(mongoUri, { useNewUrlParser: true });
+mongoose.connection.on("connected", () => {
+  console.log("CONNECTED TO MONGO INSTANCE");
+});
+mongoose.connection.on("error", (err) => {
+  console.error("Error connecting to mongo", err);
+});
 
 app.get("/", (req, res) => {
   res.send("TEST PASSED!");
