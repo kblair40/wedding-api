ea57c4ce6ed1e5aab1b76137f7cc89f66234943c
Kevin Blair <kblair40@gmail.com>
2022-05-28 07:36:08 -0500

    Move guest fetching to separate findGuest helper function

--- routes.js
+++ routes.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
+const aliasMap = require("./constants");
 const Guest = mongoose.model("Guest");
-
+console.log("ALIAS MAP:", aliasMap);
 const router = express.Router();
 
 router.post("/guest", async (req, res) => {
@@ -34,5 +34,53 @@ router.get("/guest", async (req, res) => {
     res.status(422).send({ msg: "failure" });
   }
 });
+
+const hasOtherFamily = (guest) =>
+  Boolean(guest && guest.other_family && guest.other_family.length);
+
+const hasSO = (guest) => Boolean(guest && guest.significant_other);
+
+router.get("/guest/byname", async (req, res) => {
+  // console.log("\nREQ:", req);
+  const { full_name } = req.query;
+  console.log("FULL NAME:", full_name);
+
+  let mainGuest = await findGuest(full_name);
+  if (!mainGuest) {
+    return res.status(404).send({ msg: "not found" });
+  } else {
+    return res.status(200).send(mainGuest);
+  }
+});
+
+const findGuest = async (full_name) => {
+  let foundGuest;
+  try {
+    foundGuest = await Guest.findOne({ full_name });
+    console.log("FOUND GUEST:", foundGuest);
+
+    if (foundGuest) {
+      return foundGuest;
+    } else {
+      const alias = aliasMap[full_name];
+      if (!alias) {
+        return null;
+      }
+
+      try {
+        foundGuest = await Guest.findOne({ full_name: alias });
+        console.log("FOUND GUEST:", foundGuest);
+
+        return foundGuest ? foundGuest : null;
+      } catch (e) {
+        // also unable to find using alias
+        return null;
+      }
+    }
+  } catch (e) {
+    console.log("ERROR WHILE FETCHING GUEST:", e);
+  }
+  return null;
+};
 
 module.exports = router;
