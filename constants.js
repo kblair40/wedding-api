bf9e99ae805f4b8733e08d87cff7ac0f9826bbb1
Kevin Blair <kblair40@gmail.com>
2022-05-28 07:35:51 -0500

    Add object map for aliases

---
+++ constants.js
@@ -0,0 +1,24 @@
+const aliasMap = {
+  "kathy blair": "katherine blair",
+  "daniel gerber": "dan gerber",
+  "jim appelgren": "james appelgren",
+  "theodore keller": "ted keller",
+  "liz weidner": "elizabeth weidner",
+  "grandma joyce": "joyce richardson",
+  "grandpa joe": "joe richardson",
+  "d.j. cyr": "dj cyr",
+  "cecilia bona": "cele bona",
+  "cam reichle": "cameron reichle",
+  "tayler johnson": "tayler connolly",
+  "patrick dunne": "pat dunne",
+  "daniel dunne": "dan dunne",
+  "jess dunne": "jessica dunne",
+  "andrew dunne": "andy dunne",
+  "samuel dunne": "sam dunne",
+  "jo dunne": "joellen dunne",
+  "aly stewart": "alexandra stewart",
+  "gregory gosney": "greg gosney",
+  "thomas weber": "tom weber",
+};
+
+module.exports = aliasMap;
